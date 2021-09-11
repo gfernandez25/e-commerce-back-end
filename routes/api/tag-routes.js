9b039c7072d51d793aff75100fa83fa326769989
gfernandez25 <82284313+gfernandez25@users.noreply.github.com>
2021-09-11 18:52:00 -0400

    Tag routes GET, CREATE added

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,20 +1,46 @@
 const router = require('express').Router();
-const { Tag, Product, ProductTag } = require('../../models');
+const { Tag, Product, ProductTag, Category} = require('../../models');
 
 // The `/api/tags` endpoint
 
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+    Tag.findAll({
+        include: [
+            {
+                model: Product,
+                as: "products",
+                attributes: ["id", "product_name", "price", "stock"]
+            }
+        ]
+    })
+        .then(dbTagData => res.json(dbTagData))
+        .catch(err => res.status(500).json(err))
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+    Tag.findByPk(req.params.id,{
+        include: [
+            {
+                model: Product,
+                as: "products",
+                attributes: ["id", "product_name", "price", "stock"]
+            }
+        ]
+    })
+        .then(dbTagData => res.json(dbTagData))
+        .catch(err => res.status(500).json(err))
+
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+    Tag.create(req.body)
+        .then(dbTagData => res.json(dbTagData))
+        .catch(err => res.status(500).json(err))
 });
 
 router.put('/:id', (req, res) => {
